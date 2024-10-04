4db8afab0094bc446c0a20aa3f4e1a50ed0bc827
joyiwo <fibriyanawijoyo@gmail.com>
2024-10-04 14:57:25 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis molestiae reiciendis quasi odit blanditiis maiores. Error quasi voluptatibus incidunt maiores aperiam distinctio quaerat cupiditate voluptatem est cumque, quis a magni.</h1>
 </body>
 </html>
