91e987bd03f4166ad3fc5613dbace25868358ff8
joyiwo <fibriyanawijoyo@gmail.com>
2024-10-04 15:03:14 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World!</h1>
     <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis molestiae reiciendis quasi odit blanditiis maiores. Error quasi voluptatibus incidunt maiores aperiam distinctio quaerat cupiditate voluptatem est cumque, quis a magni.</h1>
+    <ul>
+        <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus accusamus, temporibus ipsum sequi illo quis fugiat. Voluptates sunt adipisci doloremque itaque illo numquam beatae, eum ex provident harum corrupti dolorem!</li>
+    </ul>
 </body>
 </html>
